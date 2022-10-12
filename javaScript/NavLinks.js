b6274e98d8a32cf5a7250ceddaa5f79eab4bd190
gchelariu <gabrielcchelariu@gmail.com>
2022-10-12 23:12:58 +0300

    Therapy site v1.0

---
+++ javaScript/NavLinks.js
@@ -0,0 +1,11 @@
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    // navLinks.style.right = "0";
+    navLinks.style.width = "200px";
+}
+
+function hideMenu() {
+    // navLinks.style.right = "-200px"
+    navLinks.style.width = 0;
+}
